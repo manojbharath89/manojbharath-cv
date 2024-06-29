a13b27a6061d2d5bd7e37fca9550ba882a2eea28
Manoj Bharath <70174259+manojbharath89@users.noreply.github.com>
2024-06-29 15:28:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from PIL import Image
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "style" / "main.css"
-resume_file = current_dir / "assets" / "CV.pdf"
+resume_file = current_dir / "assets" / "Manoj Bharath_CV.pdf"
 profile_pic = current_dir / "assets" / "Profile.png"
 
 
